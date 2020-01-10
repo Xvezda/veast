09108aacf0d7015173e350f114bc0efb78d4f1ec
Xvezda <xvezda@naver.com>
2020-01-11 01:23:22 +0900

    Change awk to sed

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,4 +6,4 @@ set -p
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 vim -Nu vimrc +'Vader! *' +'qall!' 2>&1 \
-    | awk -f vader_filter.awk
+    | sed -ne '/^Starting Vader/,$ p'
--- tests/vader_filter.awk
+++
@@ -1,5 +0,0 @@
-BEGIN { flag = 0 }
-{
-    if (match($0, "Starting Vader") != 0) flag = 1;
-    if (flag == 1) print $0;
-}
