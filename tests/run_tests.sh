a71bd29f1921e9e5507ac1bfd678a1b1362c90f4
Xvezda <xvezda@naver.com>
2020-02-02 15:06:16 +0900

    Add group

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,4 +6,4 @@ set -p
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 vim -Nu vimrc +'Vader! *' +'qall!' 2>&1 \
-    | sed -E 's/\x1b\[[0-9~;]*[a-zA-Z]//g' | sed -n '/^Starting Vader/,$p'
+    | sed -E 's/\x1b\[([0-9~;]*[a-zA-Z])?//g' | sed -n '/^Starting Vader/,$p'
